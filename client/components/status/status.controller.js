d2abd44578fc094df5b66041b07dcb228daa11e9
Lyzzi Brooks <lyzzibrooks@Lyzzis-Air.nebula.msoe.edu>
2016-01-10 22:46:02 -0600

    more scheduling page work wooooohoooo

--- client/components/status/status.controller.js
+++ client/components/status/status.controller.js
@@ -1,18 +1,40 @@
 angular.module('myApp').controller('statusController',
 ['$scope', '$location', 'AuthService',
 function($scope, $location, AuthService) {
+  var statuses = ["Charging", "Error", "Idle", "Mowing", "Dead", "On Fire"];
+  var statusDetails = [
+    "The mower is in the charging dock.",
+    "The mower is currently stuck or is in some sort of trouble. Move it to the charging dock to reset it!",
+    "The mower is idling with the engine off, but it is not in the charging dock. Move it to the dock to charge!",
+    "The mower is currently mowing the lawn.",
+    "The mower is depleted of battery and not in the charging dock. Move it to the dock to charge!",
+    "The mower is currently on fire! You may want to put it out."
+  ];
+  var currentStatus = 0;
+
   $scope.serialnumber = "123456789";
-  $scope.status = "charging";
-  $scope.statusDetail = "The mower is in the charging dock"
+  $scope.nickname = "Brooks-Marchetti-Heck-Jet-Aleister Household";
+  $scope.status = statuses[currentStatus];
+  $scope.statusDetail = statusDetails[currentStatus];
   $scope.longitude = "43.0441100";
   $scope.latitude = "-87.9090520";
   $scope.elevation = "183.57 m";
   $scope.batterylevel = "25%";
 
-  // off, charging, mowing, idle, error
-  // The mower is depleted of battery and not in the charging dock. Move it to the dock to charge!
-  // The mower is in the charging dock.
-  // The mower is currently mowing the lawn.
-  // The mower is idling with the engine off, but it is not in the charging dock. Move it to the dock to charge!
-  // The mower is currently stuck or is in some sort of trouble. Move it to the charging dock to reset it!
+  $scope.nextStatus = function() {
+    currentStatus = (currentStatus + 1) % statuses.length;
+    $scope.status = statuses[currentStatus];
+    $scope.statusDetail = statusDetails[currentStatus];
+  };
+
+  $scope.previousStatus = function() {
+    if (currentStatus === 0) {
+      currentStatus = statuses.length;
+    } else {
+      currentStatus = (currentStatus - 1);
+    }
+    $scope.status = statuses[currentStatus];
+    $scope.statusDetail = statusDetails[currentStatus];
+  };
+
 }]);
